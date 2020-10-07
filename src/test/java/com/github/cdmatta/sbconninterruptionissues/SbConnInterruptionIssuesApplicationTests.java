e32d29b23749d422a76d8c5e3c68d65bdbde29bb
Charandeep S. Matta <charandeep.matta@gmail.com>
2020-10-07 21:50:12 +0300

    init

---
+++ src/test/java/com/github/cdmatta/sbconninterruptionissues/SbConnInterruptionIssuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cdmatta.sbconninterruptionissues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbConnInterruptionIssuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
